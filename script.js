d965884fa0d434844ebe479b9d0ff01ca1328965
Jackson-Timoteo <60791082+Jackson-Timoteo@users.noreply.github.com>
2024-07-05 12:43:49 -0300

    fix: fixing the enter btn

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ registerBtn.addEventListener('click', () => {
 });
 
 loginBtn.addEventListener('click', () => {
-    container.addEventListener.remove('active');
+    container.classList.remove('active');
 });
